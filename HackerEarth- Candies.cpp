ff52824928f11c55348058413f6ea67afd6f77f0
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-08-21 09:29:31 -0700

    Update HackerEarth- Candies.cpp

--- HackerEarth- Candies.cpp
+++ HackerEarth- Candies.cpp
@@ -19,15 +19,15 @@
                 hashing[v]++;
                 if(hashing[v]%2==0) fr++;
 
-                int left=(int)s[i-len];
+                int left=(int)s[i-len]; // right side (i) is incrementing ,so left side should be subtracted to keep n size subtring all data fixed
                 hashing[left]--;
-                if(hashing[left]%2==1) fr--;
+                if(hashing[left]%2==1) fr--;// window sliding technique ,alltime keep data of length of substring n  
             }
             if(fr>=(n/2))
                 return 1;
         }
 
-        if(fr>=n/2)
+        if(fr>=n/2) // if fr (2 times) occurs equal or more than half of the required size substring , then it can be rearranged to a palindrome of size n
         return 1;
 
         return 0;
@@ -44,7 +44,7 @@
         {
             cin>>n;
             int low=n,high=(int)s.size(),mid,ans=-1;
-            while(low<=high)
+            while(low<=high) // if n length palindrome  can't be taken from n length substring ,then we have to choose minimum length substring from where we can choose n length palindrome
             {
                 mid=(low+high)/2;
                 if(pali(mid)) ans=mid,high=mid-1;
